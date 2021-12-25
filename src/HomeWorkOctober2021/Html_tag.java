fc23d441c3207ab3ae3484e2cf35f9dc169ba5d1
MohammedRHaque <mohammedrafezhaque@gmail.com>
2021-12-24 21:26:18 -0500

    HTML WORK

---
+++ src/HomeWorkOctober2021/Html_tag.java
@@ -0,0 +1,19 @@
+package HomeWorkOctober2021;
+
+public class Html_tag {
+
+	
+	/*	
+		https://www.amazon.com/
+			This is From search Box 
+			Example:
+			//input[@id='twotabsearchtextbox']
+			(this 1 of 1 unique, as id always Unique)
+
+			https://www.amazon.com/
+			This xpath from amazon logo
+			//a[@class='nav-logo-link nav-progressive-attribute']
+
+	*/
+
+}
